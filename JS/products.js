f8ad676b78dcc552dd8a76cd3922f8222724b587
Ina Jaweed <jaweed.ina02@hotmail.com>
2023-03-16 13:52:01 +0000

    added cart page, homepage, and products

--- JS/products.js
+++ JS/products.js
@@ -2,6 +2,9 @@ const productsParent = document.querySelector(".all-products");
 const categories = document.querySelector(".categories");
 const products = data.products;
 const filters = [];
+const cartBtn = document.querySelector('.cart-btn');
+const cartSidebar = document.querySelector('.cart-sidebar');
+const cartCloseBtn = document.querySelector('.cart-close-button');
 
 categories.addEventListener("click", addFilter)
 displayProducts();
@@ -9,7 +12,8 @@ displayProducts();
 function addFilter(event) {
     removeProducts();
     const isCategoryBtn = event.target.classList.contains('category');
-    const category = event.target.textContent.toLowerCase();
+    const category = event.target.textContent.toLowerCase().replace(/\s+|&/g, '');
+
 
     if (isCategoryBtn) {
         if (!filters.includes(category)) {
@@ -24,29 +28,153 @@ function addFilter(event) {
     displayProducts();
 }
 
+
+
 function displayProducts() {
     products.forEach(product => {
-        if (filters.includes(product.category.toString()) || filters.length === 0) {
+        if (filters.length === 0 || product.category.some(c => filters.includes(c.toLowerCase().replace(/\s+|&/g, '')))) {
             const productOuterBox = document.createElement("div");
             const productInnerBox = document.createElement("img");
             const productName = document.createElement("p");
             const productPrice = document.createElement("p");
+            const addToCartBtn = document.createElement("button");
             // const productDesc = document.createElement("p");
             productsParent.appendChild(productOuterBox);
             productOuterBox.appendChild(productInnerBox);
             productOuterBox.appendChild(productName);
             productOuterBox.appendChild(productPrice);
-    
+            productOuterBox.appendChild(addToCartBtn);
+
             productOuterBox.classList.add("product-outer-box");
             productInnerBox.classList.add("product-inner-box");
             productPrice.classList.add("product-price");
-            productInnerBox.src = "../images/" + product.image;
+            addToCartBtn.classList.add("add-to-cart");
+            productInnerBox.src = "../images_products/" + product.image;
             productName.textContent = product.name;
             productPrice.textContent = "£" + product.price;
+            addToCartBtn.textContent = "Add to Basket";
+
+            addToCartBtn.addEventListener('click', () => {
+                const selectedProduct = {
+                    id: product.id,
+                    name: product.name,
+                    price: product.price
+                };
+                basket.push(selectedProduct);
+                updateBasket();
+
+                // Show success message
+                // const successMsg = document.createElement('div');
+                // successMsg.classList.add('success-msg');
+                // successMsg.textContent = `${product.name} added to cart!`;
+                // productOuterBox.appendChild(successMsg);
+                // setTimeout(() => {
+                //     successMsg.remove();
+                // }, 2000);
+            });
+
         }
     });
 }
 
 function removeProducts() {
     productsParent.innerHTML = "";
+}
+
+cartBtn.addEventListener('click', () => {
+    cartSidebar.classList.add('show');
+});
+
+cartCloseBtn.addEventListener('click', () => {
+    cartSidebar.classList.remove('show');
+});
+
+// /* ------------*/
+
+let basket = [];
+let totalPrice = 0;
+
+function updateBasket() {
+    const cartItemsContainer = document.querySelector('.cart-items-container');
+    const cartTotalPrice = document.querySelector('.cart-total-price');
+
+    // Remove all existing cart items
+    cartItemsContainer.innerHTML = '';
+
+    // Add each product in the cart to the cart items container
+    basket.forEach(product => {
+        const cartItem = document.createElement('div');
+        const cartItemName = document.createElement('p');
+        const cartItemPrice = document.createElement('p');
+        const cartItemRemoveBtn = document.createElement('button');
+
+        cartItem.classList.add('cart-item');
+        cartItemName.textContent = product.name;
+        cartItemPrice.textContent = '£' + product.price;
+        cartItemRemoveBtn.textContent = 'Remove';
+        cartItemRemoveBtn.classList.add('cart-item-remove');
+
+        cartItem.appendChild(cartItemName);
+        cartItem.appendChild(cartItemPrice);
+        cartItem.appendChild(cartItemRemoveBtn);
+
+        cartItemsContainer.appendChild(cartItem);
+    });
+
+    // Update the cart total price
+    const totalPrice = basket.reduce((acc, product) => {
+        return acc + product.price;
+    }, 0);
+    cartTotalPrice.textContent = '£' + totalPrice.toFixed(2);
+
+    // Add event listeners to remove buttons
+    const removeButtons = document.querySelectorAll('.cart-item-remove');
+    removeButtons.forEach(button => {
+        button.addEventListener('click', event => {
+            const itemName = event.target.previousSibling.previousSibling.textContent;
+            basket = basket.filter(product => product.name !== itemName);
+            updateBasket();
+        });
+    });
+}
+
+
+document.addEventListener('click', event => {
+    if (event.target.classList.contains('cart-item-remove')) {
+        const itemName = event.target.previousSibling.previousSibling.textContent;
+        basket = basket.filter(product => product.name !== itemName);
+        updateBasket();
+    }
+});
+
+
+addToCartBtn.addEventListener('click', () => {
+    const selectedProduct = {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: 1 // initialize quantity to 1 for new items
+    };
+    const index = basket.findIndex(item => item.id === selectedProduct.id);
+    if (index !== -1) {
+        basket[index].quantity++; // increase quantity if item already exists
+    } else {
+        basket.push(selectedProduct); // add new item to basket
+    }
+    updateBasket();
+});
+
+
+function removeItem(itemId) {
+    let itemIndex = cart.findIndex(item => item.id === itemId);
+    if (itemIndex !== -1) {
+        let item = cart[itemIndex];
+        if (item.quantity > 1) {
+            item.quantity--;
+        } else {
+            cart.splice(itemIndex, 1);
+        }
+        saveCart();
+    }
+    displayCart();
 }
--- data/productData.js
+++ data/productData.js
@@ -2,43 +2,204 @@ const data = {
     "products": [
         {
             "id": 0,
-            "name": "Dark Blue Jeans",
-            "price": 28.99,
-            "description": "Very comfortable, good looking trousers. Nice dark blue color.",
+            "name": "Wool-Blend Coat",
+            "price": 50.00,
+            "description": "NEED DESCRIPTION",
             "category": [
-                "trousers"
+                "Jackets & Coats"
             ],
-            "image": "darkbluejeans.png"
+            "image": "prod1.png"
         },
         {
             "id": 1,
-            "name": "Black Women Trousers",
-            "price": 22.99,
-            "description": "These women trousers are the ultimate wardrobe essential! Their sleek, sophisticated black color goes with anything and everything.",
+            "name": "Water-Repellent Puffer Jacket",
+            "price": 30.00,
+            "description": "NEED DESCRIPTION",
             "category": [
-                "trousers"
+                "Jackets&Coats"
             ],
-            "image": "blackwomentrousers.png"
+            "image": "prod2.png"
         },
         {
             "id": 2,
-            "name": "Colorful T-Shirt",
-            "price": 12.99,
-            "description": "No matter what your color preference may be - whether it's vibrant reds and yellows or darker shades - there's a colorful t-shirt available that will suit your individual style.",
+            "name": "Running Jacket",
+            "price": 24.00,
+            "description": "NEED DESCRIPTION",
             "category": [
-                "tops"
+                "Jackets & Coats"
             ],
-            "image": "colorfultshirt.png"
+            "image": "prod3.png"
         },
         {
             "id": 3,
-            "name": "Grey Running Shoes",
-            "price": 39.99,
-            "description": "Crafted with breathable fabrics and comfortable cushioning, grey running shoes provide the necessary support and comfort for your feet during high-intensity workouts.",
+            "name": "Water-Repellent Running Vest",
+            "price": 21.00,
+            "description": "NEED DESCRIPTION",
             "category": [
-                "shoes"
+                "Jackets & Coats"
             ],
-            "image": "runningshoes.png"
-        }
+            "image": "prod4.png"
+        },
+        {
+            "id": 4,
+            "name": "3-Layer Shell Jacket",
+            "price": 60.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Jackets & Coats"
+            ],
+            "image": "prod9.png"
+        },
+        {
+            "id": 5,
+            "name": "Hybrid Regular Jeans",
+            "price": 20.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Jeans"
+            ],
+            "image": "prod6.png"
+        },
+        {
+            "id": 6,
+            "name": "Skinny Jeans",
+            "price": 21.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Jeans"
+            ],
+            "image": "prod7.png"
+        },
+        {
+            "id": 7,
+            "name": "Lose Pull-On Jeans",
+            "price": 21.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Jeans"
+            ],
+            "image": "prod8.png"
+        },
+        {
+            "id": 8,
+            "name": "Relaxed Jeans",
+            "price": 20.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Jeans"
+            ],
+            "image": "prod5.png"
+        },
+        {
+            "id": 9,
+            "name": "Loose Jeans",
+            "price": 20.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Jeans"
+            ],
+            "image": "prod10.png"
+        },
+        {
+            "id": 10,
+            "name": "Oversized Hoodie",
+            "price": 16.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Hoodies & Sweatshirts"
+            ],
+            "image": "prod11.png"
+        },
+        {
+            "id": 11,
+            "name": "Regular Hoodie",
+            "price": 9.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Hoodies & Sweatshirts"
+            ],
+            "image": "prod12.png"
+        },
+        {
+            "id": 12,
+            "name": "Oversized-Zip Sweatshirt",
+            "price": 12.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Hoodies & Sweatshirts"
+            ],
+            "image": "prod13.png"
+        },
+        {
+            "id": 13,
+            "name": "Relaxed-Fit Sweatshirt",
+            "price": 11.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Hoodies & Sweatshirts"
+            ],
+            "image": "prod14.png"
+        },
+        {
+            "id": 14,
+            "name": "Printed Hoodie",
+            "price": 15.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Hoodies & Sweatshirts"
+            ],
+            "image": "prod15.png"
+        },
+        {
+            "id": 15,
+            "name": "Running Shorts",
+            "price": 10.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Shorts"
+            ],
+            "image": "prod16.png"
+        },
+        {
+            "id": 16,
+            "name": "Relaxed Fit Shorts",
+            "price": 9.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Shorts"
+            ],
+            "image": "prod17.png"
+        },
+        {
+            "id": 17,
+            "name": "Regular Denim Short",
+            "price": 15.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Shorts"
+            ],
+            "image": "prod18.png"
+        },
+        {
+            "id": 18,
+            "name": "Linen Shorts",
+            "price": 18.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Shorts"
+            ],
+            "image": "prod19.png"
+        },
+        {
+            "id": 19,
+            "name": "Padded Nylon Shorts",
+            "price": 40.00,
+            "description": "NEED DESCRIPTION",
+            "category": [
+                "Shorts"
+            ],
+            "image": "prod20.png"
+        },
+
     ]
 }
